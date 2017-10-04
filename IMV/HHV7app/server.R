d53690aed0d226c79c13fcfa17a4ce88ef5dfa9d
mihuber <mihuber@me.com>
2017-10-04 17:51:01 +0200

    added tabs in layout

--- IMV/HHV7app/server.R
+++ IMV/HHV7app/server.R
@@ -1,75 +1,75 @@
 ### HHV7app
 
 library(shiny)
-library(tidyr)
-library(dplyr)
-library(ggplot2)
-library(cowplot)
-library(readr)
+library(tidyverse)
 
 shinyServer(function(input, output, session) {
         
-        data <- reactive({
-               read_csv("sample_results.csv") %>%
-                        mutate(group = as.character(group))
-                })
+        data = reactive({read_csv("sample_results.csv") %>% mutate(group = as.character(group))})
         
-        observeEvent(input$all_types, {
-                updateCheckboxGroupInput(session=session, "sample_type", selected = c("Blood", "Throat swab", "Urine"))
-                })
+        samples_found = reactive({data() %>% arrange(sample_name) %>% pull(sample_name) %>% unique()})
+        students_found = reactive({data() %>% arrange(student) %>% pull(student) %>% unique()})
         
+        observeEvent(input$select_all_sample_types, {updateCheckboxGroupInput(session=session, "sample_type", selected = c("Blood", "Throat swab", "Urine"))})
         
-        output$samples_found <- renderUI({
-                s = data() %>% arrange(sample_name) %>% pull(sample_name) %>% unique() 
-                selectInput("sample_name", "Sample Names", s, multiple=TRUE, selectize=FALSE, selected = s)
-                })
-        observeEvent(input$all_samples, {
-                s = data() %>% arrange(sample_name) %>% pull(sample_name) %>% unique() 
-                updateSelectInput(session=session, "sample_name", selected = s)
-                })
+        output$sample_selection = renderUI({selectInput("sample_name", "Sample Names", samples_found(), multiple = TRUE, selectize = FALSE, selected = samples_found())})
+        observeEvent(input$select_all_samples, {updateSelectInput(session=session, "sample_name", selected = samples_found())})
         
+        output$student_selection = renderUI({selectInput("student", "Students", students_found(), multiple = TRUE, selectize = FALSE, selected = students_found())})
+        observeEvent(input$select_all_students, {updateSelectInput(session=session, "student", selected = students_found())})
         
-        output$students_found <- renderUI({
-                s = data() %>% arrange(student) %>% pull(student) %>% unique() 
-                selectInput("student", "Student", s, multiple=TRUE, selectize=FALSE, selected = s)
-                })
-        observeEvent(input$all_students, {
-                s = data() %>% arrange(student) %>% pull(student) %>% unique()
-                updateSelectInput(session=session, "student", selected = s)
-                })
-        
-        
-        output$plot = renderPlot({
-                req(input$sample_name)
-                data = data() %>%
+        plot_data = reactive({data() %>%
                         filter(sample_type %in% input$sample_type) %>%
                         filter(sample_name %in% input$sample_name) %>%
                         filter(student %in% input$student)
-                if (input$comparison == "sample_type") {
-                        p = ggplot(data, aes(x = sample_type, y = HHV7, color = sample_type))
-                } else if (input$comparison == "sex") {
-                        p = ggplot(data, aes(x = sex, y = HHV7, color = sex))
-                } else if (input$comparison == "group") {
-                        p = ggplot(data, aes(x = group, y = HHV7, color = group))
-                } else if (input$comparison == "symptoms") {
-                        p = ggplot(data, aes(x = group, y = HHV7, color = symptoms))
-                } else if (input$comparison == "age") {
-                        p = ggplot(data, aes(x = age, y = HHV7)) +
-                                geom_point() +
-                                geom_smooth(method = lm, se = FALSE, fullrange = TRUE)
-                        }
-                
-                if (input$comparison != "age") {
-                        p = p + geom_boxplot(outlier.color = "white") +
-                                geom_jitter(height = 0, width = 0.2, size = 4)
-                        }
-                
-                p = p + facet_grid(. ~ sample_type, scales = "free") +
-                        ylab("HHV-7 copies/ml") +
-                        theme(legend.position="none") +
-                        theme(axis.text = element_text(size = 15)) +
-                        theme(axis.title = element_text(size = 20, face = "bold")) 
-                
+                })
+        
+        plot_labels = list(ylab("HHV-7 copies/ml"))
+        plot_theme = theme(legend.position="none", axis.text=element_text(size = 15), axis.title=element_text(size = 20, face = "bold"))
+        
+        output$plot_sample = renderPlot({
+                p = ggplot(plot_data(), aes(x = sample_type, y = HHV7, color = sample_type, fill = sample_type)) +
+                        geom_boxplot(outlier.color = "white", alpha = 0.1) +
+                        geom_jitter(height = 0, width = 0.2, size = 4) +
+                        facet_grid(. ~ sample_type, scales = "free") +
+                        xlab("Sample type")
+                p = p + plot_labels + plot_theme
+                return(p)
+                })
+        
+        output$plot_sex = renderPlot({
+                p = ggplot(plot_data(), aes(x = sex, y = HHV7, color = sex, fill = sex)) +
+                        geom_boxplot(outlier.color = "white", alpha = 0.1) +
+                        geom_jitter(height = 0, width = 0.2, size = 4) +
+                        facet_grid(. ~ sample_type) +
+                        xlab("Sex")
+                p = p + plot_labels + plot_theme
+                return(p)
+                })
+        
+        output$plot_group = renderPlot({
+                p = ggplot(plot_data(), aes(x = group, y = HHV7, color = group, fill = group)) +
+                        geom_boxplot(outlier.color = "white", alpha = 0.1) +
+                        geom_jitter(height = 0, width = 0.2, size = 4) +
+                        facet_grid(. ~ sample_type) +
+                        xlab("Student group")
+                p = p + plot_labels + plot_theme
+                return(p)
+                })
+        
+        output$plot_symptoms = renderPlot({
+                #p = ggplot
+                #return(p)
+                #p = p + plot_labels + plot_theme
+                })
+        
+        output$plot_age = renderPlot({
+                p = ggplot(plot_data(), aes(x = age, y = HHV7)) +
+                        geom_point(size = 4) +
+                        geom_smooth(method = lm, se = FALSE, fullrange = TRUE) +
+                        facet_grid(. ~ sample_type) +
+                        xlab("Age")
+                p = p + plot_labels + plot_theme
                 return(p)
                 })
 })
--- IMV/HHV7app/ui.R
+++ IMV/HHV7app/ui.R
@@ -1,27 +1,48 @@
+### HHV7app
+
 library(shiny)
 library(shinythemes)
 
 shinyUI(fluidPage(
-        theme = shinytheme("cerulean"),
+        #theme = shinytheme("cerulean"),
+        
         titlePanel("HHV7app"),
-        fluidRow(
-                column(2,
-                       wellPanel(
-                               checkboxGroupInput("sample_type", "Sample Type", c("Blood", "Throat swab", "Urine"), selected = c("Blood", "Throat swab", "Urine")),
-                               actionButton("all_types", "Select all"),
-                               hr(),
-                               uiOutput("samples_found"),
-                               actionButton("all_samples", "Select all"),
-                               hr(),
-                               uiOutput("students_found"),
-                               actionButton("all_students", "Select all"),
-                               hr(),
-                               radioButtons("comparison", "Comparison", c("Sample Type" = "sample_type", "Sex" = "sex", "Age" = "age", "Group" = "group", "Symptoms" = "symptoms"), selected = "sample_type")
-                               )
-                       ),
-                column(10,
-                       plotOutput("plot", height = 800)
+        sidebarLayout(
+                
+                sidebarPanel(
+                        checkboxGroupInput("sample_type", "Sample Types", c("Blood", "Throat swab", "Urine"), selected = c("Blood", "Throat swab", "Urine")),
+                        actionButton("select_all_sample_types", "Select all"),
+                        hr(),
+                        uiOutput("sample_selection"),
+                        actionButton("select_all_samples", "Select all"),
+                        hr(),
+                        uiOutput("student_selection"),
+                        actionButton("select_all_students", "Select all"),
+                        width = 2),
+                
+                mainPanel(
+                        tabsetPanel(
+                                tabPanel("Sample",
+                                         verticalLayout(h2("HHV-7 concentrations in different sample types"),
+                                                        plotOutput("plot_sample", height = 800)
+                                                        )),
+                                tabPanel("Sex",
+                                         verticalLayout(h2("HHV-7 concentrations in male and female individuals"),
+                                                        plotOutput("plot_sex", height = 800)
+                                         )),
+                                tabPanel("Age",
+                                         verticalLayout(h2("HHV-7 concentrations depending on age"),
+                                                        plotOutput("plot_age", height = 800)
+                                         )),
+                                tabPanel("Symptoms",
+                                         verticalLayout(h2("HHV-7 concentrations depending on symptoms"),
+                                                        plotOutput("plot_symtpoms", height = 800)
+                                         )),
+                                tabPanel("Group",
+                                         verticalLayout(h2("HHV-7 concentrations measured by the four student groups"),
+                                                        plotOutput("plot_group", height = 800)
+                                         ))
+                                )
                         )
                 )
-        )
-)
+))
