cf2d8a30dc2d9bcc04f30b90d272760327110971
mihuber <mihuber@me.com>
2018-02-09 14:44:01 +0100

    added downloads

--- DIA/Statistik/app.R
+++ DIA/Statistik/app.R
@@ -1,6 +1,7 @@
 library(shiny)
 library(tidyverse)
 library(readxl)
+library(cowplot)
 
 master = read_excel("Mastertabelle_Analysestatistik.xlsx")
 options(shiny.maxRequestSize = 100*1024^2)
@@ -11,9 +12,16 @@ ui <- fluidPage(
         titlePanel("Diagnostik Statistik"),
         sidebarLayout(
                 sidebarPanel(
-                        fileInput("molis_file", "MOLIS Export")
+                        fileInput("molis_file", "MOLIS Export", accept = c(".xls", ".xlsx", ".csv")),
+                        h3(),
+                        downloadButton("downloadBereich", "Download Statistik Arbeitsbereich"),
+                        h5(),
+                        downloadButton("downloadVerfahren", "Download Statistik Verfahren"),
+                        h5(),
+                        downloadButton("downloadMaster", "Download Mastertabelle")
                 ),
                 mainPanel(
+                        plotOutput(outputId = "Summary"),
                         h3("Arbeitsbereiche"),
                         tableOutput(outputId = "Bereich"),
                         h3("Analysen"),
@@ -26,7 +34,13 @@ ui <- fluidPage(
 server <- function(input, output) {
         data = reactive({
                 req(input$molis_file)
-                read_excel(input$molis_file$datapath) %>%
+                if (grepl(".csv", input$molis_file)) {
+                        raw_data = read_csv(input$molis_file$datapath)
+                }
+                else if (grepl(".xls", input$molis_file)) {
+                        raw_data = read_excel(input$molis_file$datapath)
+                }
+                raw_data %>%
                         select(Einsender, MC) %>%
                         rename(Code = MC) %>%
                         full_join(. , master) %>%
@@ -37,9 +51,7 @@ server <- function(input, output) {
                         mutate(Code = ifelse(Einsender == "HIVANO" & Code == "H06UST", "XXX", Code))
         })
         
-        output$Bereich = renderTable({
-                req(input$molis_file)
-                print(data())
+        Bereich = reactive({
                 data() %>%
                         mutate(type = paste(Arbeitsbereich, Unterbereich, Weitere_Unterteilung)) %>%
                         group_by(type) %>%
@@ -50,10 +62,10 @@ server <- function(input, output) {
                         ungroup() %>%
                         mutate(Gesamttotal = sum(Subtotal)) %>%
                         select(-Einsender, -type, -Code, -Pathogen, -Analyse)
+                
         })
         
-        output$Verfahren = renderTable({
-                req(input$molis_file)
+        Verfahren = reactive({
                 data() %>%
                         group_by(Code) %>%
                         mutate(Total = n()) %>%
@@ -61,6 +73,53 @@ server <- function(input, output) {
                         ungroup() %>%
                         select(-Einsender, -Code, -Arbeitsbereich, -Unterbereich, -Weitere_Unterteilung)
         })
+        
+        
+        output$Bereich = renderTable({
+                req(input$molis_file)
+                Bereich()
+        })
+        
+        output$Verfahren = renderTable({
+                req(input$molis_file)
+                Verfahren()
+        })
+        
+        output$Summary = renderPlot({
+                req(input$molis_file)
+                Bereich() %>%
+                        ggplot(aes(x = Unterbereich, y = Subtotal ))+ #, fill = Weitere_Unterteilung)) +
+                        geom_bar(stat="identity") +
+                        facet_grid(. ~ Arbeitsbereich, scales = "free") +
+                        theme(axis.text.x  = element_text(angle = 90, hjust = 1)) +
+                        panel_border() +
+                        background_grid(major = "xy") +
+                        xlab("")
+        })
+        
+        output$downloadBereich <- downloadHandler(
+                filename = function() {
+                        paste(sub("(.*?)\\..*$", "\\1", input$molis_file), "_Bereich.csv", sep = "")
+                },
+                content = function(file) {
+                        write.csv(Bereich(), file, row.names = FALSE)
+        })
+        
+        output$downloadVerfahren <- downloadHandler(
+                filename = function() {
+                        paste(sub("(.*?)\\..*$", "\\1", input$molis_file), "_Verfahren.csv", sep = "")
+                },
+                content = function(file) {
+                        write.csv(Verfahren(), file, row.names = FALSE)
+                })
+        
+        output$downloadMaster <- downloadHandler(
+                filename = function() {
+                        paste("Mastertabelle_Analysestatistik.csv", sep = "")
+                },
+                content = function(file) {
+                        write.csv(master, file, row.names = FALSE)
+                })
 }
 
 ### RUN APP ###
