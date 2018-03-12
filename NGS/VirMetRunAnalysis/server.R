d02b245e197a81eceee0edfb4418c5b65195b139
Michael Huber <huber.michael@virologymc18.local>
2018-03-12 11:51:26 +0100

    added hover function to select sscinames by species

--- NGS/VirMetRunAnalysis/server.R
+++ NGS/VirMetRunAnalysis/server.R
@@ -8,91 +8,91 @@ library(DT)
 #library(shinythemes)
 
 shinyServer(function(input, output) {
-        
-        reads_data <- reactive({
-                req(input$reads_file)
-                read_delim(input$reads_file$datapath, "\t")
-                })
-        
-        orgs_data <- reactive({
-                req(input$orgs_file)
-                read.delim(input$orgs_file$datapath, sep = "\t") %>%
-                  mutate(covered_percent = 100 * covered_region / seq_len) %>%
-                  mutate(covered_max = 100 * reads * 151 / seq_len) %>%
-                  mutate(covered_max = if_else(covered_max > 100, 100, covered_max)) %>%
-                  mutate(covered_score = round(100 * covered_percent / covered_max, 1)) %>%
-                  mutate(covered_percent = round(covered_percent, 1))
-                })
-       
-        output$plot_run <- renderPlot({
-                req(!(is.null(input$chosen_sample)))
-                reads_data() %>%
-                        filter(sample %in% input$chosen_sample) %>%
-                        filter(category == "raw_reads" | category == "passing_filter") %>%
-                        mutate(quality = factor(category, levels = c("raw_reads", "passing_filter"))) %>%
-                        ### plot 
-                        ggplot(aes(x = quality, y = reads, fill = quality)) +
-                        geom_col(colour = "black") +
-                        scale_fill_manual(name = "",
-                                          values = c("#D55E00", "#009E73")) +
-                        panel_border() + background_grid(major = "xy", minor = "") +
-                        xlab("") + ylab("") +
-                        facet_wrap( ~ sample) +
-                        theme(legend.position = "bottom") + theme(legend.title = element_blank()) +
-                        theme(axis.text.x  = element_text(angle = 90, vjust = 0.4, hjust = 1)) +
-                        scale_x_discrete(breaks = NULL)
-                })
-        
-        output$plot_domain <- renderPlot({
-                req(!(is.null(input$chosen_sample)))
-                reads_data() %>%
-                        filter(sample %in% input$chosen_sample) %>%
-                        filter(!(category == "raw_reads" | category == "passing_filter" | category == "reads_to_blast")) %>%
-                        mutate(domain = case_when(
-                                category %in% c("matching_bact1", "matching_bact2", "matching_bact3") ~ "bacterial",
-                                category %in% c("viral_reads") ~ "viral",
-                                category %in% c("matching_humanGRCh38") ~ "human",
-                                category %in% c("undetermined_reads") ~ "unknown",
-                                category %in% c("matching_bt_ref") ~ "bovine",
-                                category %in% c("matching_fungi1") ~ "fungal",
-                                TRUE ~ "none"
-                                )) %>%
-                        mutate(domain = factor(as.character(domain), levels = c("human", "bacterial", "fungal", "bovine", "viral", "unknown"))) %>%
-                        filter(!(domain == "none")) %>%
-                        group_by(sample) %>%
-                        mutate(percent = reads/sum(reads)) %>%
-                        ### plot        
-                        ggplot(aes(x = domain, y = percent, fill = domain)) +
-                        geom_col(colour = "black") +
-                        scale_fill_manual(name = "",
-                                          values = c("#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00")) +
-                        panel_border() + background_grid(major = "xy", minor = "") +
-                        xlab("") + ylab("") +
-                        facet_wrap( ~ sample) +
-                        theme(legend.position = "bottom") + theme(legend.title = element_blank()) +
-                        theme(axis.text.x  = element_text(angle = 90, vjust = 0.4, hjust = 1)) + 
-                        scale_x_discrete(breaks = NULL)
-                })
-        
-        output$table_ssciname <- DT::renderDataTable(
-          filter = "top",
-          rownames = FALSE,
-          options = list(pageLength = 100, autoWidth = FALSE),
-          {
-          req(!(is.null(input$chosen_sample)))
-          orgs_data() %>%
+    
+    reads_data <- reactive({
+        req(input$reads_file)
+        read_delim(input$reads_file$datapath, "\t")
+    })
+    
+    orgs_data <- reactive({
+        req(input$orgs_file)
+        read.delim(input$orgs_file$datapath, sep = "\t") %>%
+            mutate(covered_percent = 100 * covered_region / seq_len) %>%
+            mutate(covered_max = 100 * reads * 151 / seq_len) %>%
+            mutate(covered_max = if_else(covered_max > 100, 100, covered_max)) %>%
+            mutate(covered_score = round(100 * covered_percent / covered_max, 1)) %>%
+            mutate(covered_percent = round(covered_percent, 1)) #%>%
+            #mutate(rpkm = reads / (seq_len / 1000 * passing_filter / 1000000))
+    })
+    
+    output$plot_run <- renderPlot({
+        req(!(is.null(input$chosen_sample)))
+        reads_data() %>%
             filter(sample %in% input$chosen_sample) %>%
-            select(ssciname, species, reads, covered_percent, covered_score, sample) %>%
-            arrange(desc(reads))
-          })
-
-        output$table_species <- DT::renderDataTable(
-          filter = "top",
-          rownames = FALSE,
-          options = list(pageLength = 100, autoWidth = FALSE),
-          {
-          req(!(is.null(input$chosen_sample)))
-          orgs_data() %>%
+            filter(category == "raw_reads" | category == "passing_filter") %>%
+            mutate(quality = factor(category, levels = c("raw_reads", "passing_filter"))) %>%
+            
+            ### plot 
+            ggplot(aes(x = quality, y = reads, fill = quality)) +
+            geom_col(colour = "black") +
+            scale_fill_manual(name = "",
+                              values = c("#D55E00", "#009E73")) +
+            panel_border() + background_grid(major = "xy", minor = "") +
+            xlab("") + ylab("") +
+            facet_wrap( ~ sample) +
+            theme(legend.position = "bottom") + theme(legend.title = element_blank()) +
+            theme(axis.text.x  = element_text(angle = 90, vjust = 0.4, hjust = 1)) +
+            scale_x_discrete(breaks = NULL)
+    })
+    
+    output$plot_domain <- renderPlot({
+        req(!(is.null(input$chosen_sample)))
+        reads_data() %>%
+            filter(sample %in% input$chosen_sample) %>%
+            filter(!(category == "raw_reads" | category == "passing_filter" | category == "reads_to_blast")) %>%
+            mutate(domain = case_when(
+                category %in% c("matching_bact1", "matching_bact2", "matching_bact3") ~ "bacterial",
+                category %in% c("viral_reads") ~ "viral",
+                category %in% c("matching_humanGRCh38") ~ "human",
+                category %in% c("undetermined_reads") ~ "unknown",
+                category %in% c("matching_bt_ref") ~ "bovine",
+                category %in% c("matching_fungi1") ~ "fungal",
+                TRUE ~ "none"
+            )) %>%
+            mutate(domain = factor(as.character(domain), levels = c("human", "bacterial", "fungal", "bovine", "viral", "unknown"))) %>%
+            filter(!(domain == "none")) %>%
+            group_by(sample) %>%
+            mutate(percent = reads/sum(reads)) %>%
+            
+            ### plot        
+            ggplot(aes(x = domain, y = percent, fill = domain)) +
+            geom_col(colour = "black") +
+            scale_fill_manual(name = "",
+                              values = c("#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00")) +
+            panel_border() + background_grid(major = "xy", minor = "") +
+            xlab("") + ylab("") +
+            facet_wrap( ~ sample) +
+            theme(legend.position = "bottom") + theme(legend.title = element_blank()) +
+            theme(axis.text.x  = element_text(angle = 90, vjust = 0.4, hjust = 1)) + 
+            scale_x_discrete(breaks = NULL)
+    })
+    
+    output$table_ssciname <- DT::renderDataTable(
+        filter = "top",
+        rownames = FALSE,
+        options = list(pageLength = 100, autoWidth = FALSE),
+        {
+            req(!(is.null(input$chosen_sample)))
+            orgs_data() %>%
+                filter(sample %in% input$chosen_sample) %>%
+                filter(species %in% species_selected()) %>%
+                select(ssciname, reads, covered_percent, covered_score, sample) %>%
+                arrange(desc(reads))
+        })
+    
+    table_species <- reactive({
+        req(!(is.null(input$chosen_sample)))
+        orgs_data() %>%
             filter(sample %in% input$chosen_sample) %>%
             select(species, reads, sample) %>%
             group_by(species, sample) %>%
@@ -107,38 +107,58 @@ shinyServer(function(input, output) {
             arrange(desc(reads_total)) %>%
             arrange(occurence) %>%
             select(-percent, -abundance, -occurence)
-          })
-
-        output$report <- downloadHandler(
-                filename = function() {
-                        paste0(substr(input$chosen_sample[1], 1, 13), ".pdf")
-                        },
-                content = function(file) {
-                        tempReport <- file.path(tempdir(), "report.Rmd")
-                        file.copy("RunReport.Rmd", tempReport, overwrite = TRUE)
-                        
-                        params <- list(orgs_file = input$orgs_file$datapath,
-                                       reads_file = input$reads_file$datapath,
-                                       sample_name = input$chosen_sample,
-                                       rows_selected = input$table_species_rows_selected)
-                        
-                        rmarkdown::render(tempReport, output_file = file,
-                                          params = params,
-                                          envir = new.env(parent = globalenv()))
-                })
-        
-        output$samples_found <- renderUI({
-          found_samples <- reads_data() %>% arrange(sample) %>% .$sample %>% unique() 
-          selectInput("chosen_sample", "Choose one or more samples:", found_samples, multiple = TRUE, selectize = FALSE, size = 10)
-          })
-        
-        
-        output$check_tsv <- renderText({
-          if (reads_data()$run[1] != orgs_data()$run[1]) {"Run IDs do not match!"}
-          })
-        
-        
-        output$check_sample <- renderText({
-          if (length(unique(substr(input$chosen_sample, 1, 10))) > 1) {"More than one sample selected!"}
-          })
+    })
+    
+    species_selected <- reactive({
+        table_species()[input$hoverIndexJS + 1, ] %>%
+            pull(species)
+    })
+    
+    output$table_species <- DT::renderDataTable(
+        filter = "top",
+        rownames = FALSE,
+        options = list(pageLength = 100,
+                       autoWidth = FALSE,
+                       rowCallback = JS('function(row, data) {
+                                                  $(row).mouseenter(function(){
+                                                  var hover_index = $(this)[0]._DT_RowIndex
+                                                  /* console.log(hover_index); */
+                                                  Shiny.onInputChange("hoverIndexJS", hover_index);
+                                                  });
+                                          }')
+        ), 
+        {
+            table_species()
+        })
+    
+    output$report <- downloadHandler(
+        filename = function() {
+            paste0(substr(input$chosen_sample[1], 1, 13), ".pdf")
+        },
+        content = function(file) {
+            tempReport <- file.path(tempdir(), "report.Rmd")
+            file.copy("RunReport.Rmd", tempReport, overwrite = TRUE)
+            
+            params <- list(orgs_file = input$orgs_file$datapath,
+                           reads_file = input$reads_file$datapath,
+                           sample_name = input$chosen_sample,
+                           rows_selected = input$table_species_rows_selected)
+            
+            rmarkdown::render(tempReport, output_file = file,
+                              params = params,
+                              envir = new.env(parent = globalenv()))
+        })
+    
+    output$samples_found <- renderUI({
+        found_samples <- reads_data() %>% arrange(sample) %>% .$sample %>% unique() 
+        selectInput("chosen_sample", "Choose one or more samples:", found_samples, multiple = TRUE, selectize = FALSE, size = 10)
+    })
+    
+    output$check_tsv <- renderText({
+        if (reads_data()$run[1] != orgs_data()$run[1]) {"Run IDs do not match!"}
+    })
+    
+    output$check_sample <- renderText({
+        if (length(unique(substr(input$chosen_sample, 1, 10))) > 1) {"More than one sample selected!"}
+    })
 })
--- NGS/VirMetRunAnalysis/ui.R
+++ NGS/VirMetRunAnalysis/ui.R
@@ -10,40 +10,38 @@ shinyUI(fluidPage(
                        wellPanel(
                                fileInput("reads_file", "Select run_reads_summary.tsv file:", accept = ".tsv"),
                                fileInput("orgs_file", "Select orgs_species_found.tsv file:", accept = ".tsv"),
-                               span(h4(textOutput("check_tsv")), style="color:red")
-                       )
-                ),
+                               span(h4(textOutput("check_tsv")), style = "color:red")
+                       )),
                 column(4,
-                       wellPanel(
-                               uiOutput("samples_found")
-                       )
-                ),
+                       wellPanel(uiOutput("samples_found"))),
                 column(4,
                        wellPanel(
                                downloadButton("report", "Generate report"),
-                               span(h4(textOutput("check_sample")), style="color:red")
-                       )
-                )
+                               span(h4(textOutput("check_sample")), style = "color:red")
+                       ))
         ),
         hr(),
+        fluidRow(column(
+                6,
+                h3("Number of raw and quality filtered reads"),
+                plotOutput(outputId = "plot_run")
+        ),
+        column(
+                6,
+                h3("Distribution of sequencing reads into taxonomic categories"),
+                plotOutput(outputId = "plot_domain")
+        )),
+        hr(),
         fluidRow(
-                column(6,
-                       h3("Number of raw and quality filtered reads"),
-                       plotOutput(outputId = "plot_run")
+                column(
+                        6,
+                        h3("Detected viruses on species level"),
+                        DT::dataTableOutput("table_species")
                 ),
-                column(6,
-                       h3("Distribution of sequencing reads into taxonomic categories"),
-                       plotOutput(outputId = "plot_domain")
-                )
-        ),
-        hr(),
-        fluidRow(
-                column(6,
-                       tabPanel("Detected virus species", DT::dataTableOutput("table_species"))),
-                column(6,
-                       tabPanel("Detected virus sscinames", DT::dataTableOutput("table_ssciname")
-                       )
+                column(
+                        6,
+                        h3("Corresponding virus reference sequences"),
+                        DT::dataTableOutput("table_ssciname")
                 )
         )
-)
-)
+))
