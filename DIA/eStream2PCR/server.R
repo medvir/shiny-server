ee081aa46143f5d7943743ac33f0a3a8c802011a
Michael Huber <huber.michael@virologymc18.local>
2018-07-20 14:43:41 +0200

    added stup file for target/reporter/quencher combination, target colors selected as sample colors

--- DIA/eStream2PCR/server.R
+++ DIA/eStream2PCR/server.R
@@ -4,6 +4,16 @@ library(shiny)
 library(tidyverse)
 library(stringr)
 library(DT)
+library(readr)
+
+### setup
+setup = read_csv("setup.csv")
+
+reporter = setup$reporter
+names(reporter) = setup$target
+
+quencher = setup$quencher
+names(quencher) = setup$target
 
 well_dict = 1:96
 names(well_dict) = c("A1", "A2", "A3", "A4", "A5", "A6", "A7", "A8", "A9", "A10", "A11", "A12",
@@ -15,9 +25,12 @@ names(well_dict) = c("A1", "A2", "A3", "A4", "A5", "A6", "A7", "A8", "A9", "A10"
                      "G1", "G2", "G3", "G4", "G5", "G6", "G7", "G8", "G9", "G10", "G11", "G12",
                      "H1", "H2", "H3", "H4", "H5",  "H6", "H7","H8", "H9","H10", "H11", "H12")
 
+
+### color functions
 rgb2hex <- function(v) {
     sapply(strsplit(as.character(v), "RGB\\(|,|)"), function(x) rgb(x[2], x[3], x[4], maxColorValue=255))
 }
+
 
 rgb_color_rainbow <- function(n, s, v) {
     col = rainbow(n, s, v, start = 0, end = max(1, n - 1)/n)
@@ -26,12 +39,14 @@ rgb_color_rainbow <- function(n, s, v) {
     return(rgb)
 }
 
+
 rgb_color_repeat <- function(n) {
     col = c("RGB(230,159,0)", "RGB(86,180,233)", "RGB(0,158,115)", "RGB(240,228,66)", "RGB(0,114,178)", "RGB(213,94,0)", "RGB(204,121,167)", "RGB(153,153,153)")
     rgb = rep(col, length.out = n)
     names(rgb) = 1:n
     return(rgb)
 }
+
 
 rgb_color_random <- function(n, s, v) {
     rgb = vector(length = n)
@@ -65,30 +80,21 @@ shinyServer(function(input, output, session) {
                 grepl("BK", target) ~ "BK",
                 grepl("GAPDH", target) ~ "GAPDH",
                 grepl("Lambda", target) ~ "Lambda",
+                grepl("HHV-6", target) ~ "HHV-6",
+                grepl("VZV", target) ~ "VZV",
+                grepl("HSV-1", target) ~ "HSV-1",
+                grepl("HSV-2", target) ~ "HSV-2",
+                grepl("Parvo", target) ~ "Parvo",
+                grepl("Adeno", target) ~ "Adeno",
+                grepl("JC", target) ~ "JC",
                 TRUE ~ "unknown"
             )) %>%
             
             separate(Well, into = c("pos", "Well Position"), sep = "\\.") %>%
             select(-pos) %>%
             mutate(Well = well_dict[`Well Position`]) %>%
-            
-            mutate(Reporter	= case_when(
-                `Target Name` == "CMV" ~ "FAM",
-                `Target Name` == "EBV" ~ "FAM",
-                `Target Name` == "BK" ~ "FAM",
-                `Target Name` == "GAPDH" ~ "VIC",
-                `Target Name` == "Lambda" ~ "FAM",
-                TRUE ~ "unknown"
-            )) %>%
-            
-            mutate(`Target Color` = case_when(
-                `Target Name` == "CMV" ~ "RGB(230,159,0)",
-                `Target Name` == "EBV" ~ "RGB(86,180,233)",
-                `Target Name` == "BK" ~ "RGB(0,158,115)",
-                `Target Name` == "GAPDH" ~ "RGB(213,94,0)",
-                `Target Name` == "Lambda" ~ "RGB(204,121,167)",
-                TRUE ~ "RGB(0,0,0)"
-            )) 
+            mutate(Reporter	= reporter[`Target Name`]) %>%
+            mutate(Quencher = quencher[`Target Name`])
     })
     
     
@@ -96,50 +102,60 @@ shinyServer(function(input, output, session) {
         
         dat = eStream_data() %>%
             filter(`Target Name` %in% input$targets) %>%
-            mutate(sample_color_id = group_indices(.,`Sample Name`))
+            mutate(sample_color_id = group_indices(.,`Sample Name`)) %>%
+            mutate(target_color_id = group_indices(.,`Target Name`))
         
         if (input$colors == "repeat") {
             sample_colors = rgb_color_repeat(max(dat$sample_color_id))
+            target_colors = rgb_color_repeat(max(dat$target_color_id))
         } else if (input$colors == "random") {
             sample_colors = rgb_color_random(max(dat$sample_color_id), input$saturation, input$value)
+            target_colors = rgb_color_random(max(dat$target_color_id), input$saturation, input$value)
         } else {
             sample_colors = rgb_color_rainbow(max(dat$sample_color_id), input$saturation, input$value)
+            target_colors = rgb_color_rainbow(max(dat$target_color_id), input$saturation, input$value)
         } 
         
         dat %>%
             mutate(`Sample Color` = sample_colors[sample_color_id]) %>%
-            mutate(Quencher = "TAMRA") %>%
-            mutate(Task = "UNKNOWN") %>%
-            mutate('Biogroup Name' = "", 'Biogroup Color' = "", Quantity = "", Comments = "") %>%
+            mutate(`Target Color` = target_colors[target_color_id]) %>%
+            mutate(Task = "UNKNOWN", 'Biogroup Name' = "", 'Biogroup Color' = "", Quantity = "", Comments = "") %>%
             arrange(Well) %>%
-            select(Well, `Well Position`, `Sample Name`, `Sample Color`, `Biogroup Name`, `Biogroup Color`, `Target Name`, `Target Color`, Task,	Reporter, Quencher, Quantity, Comments)
+            select(Well, `Well Position`, `Sample Name`, `Sample Color`, `Biogroup Name`, `Biogroup Color`, `Target Name`, `Target Color`, Task, Reporter, Quencher, Quantity, Comments)
     })
+    
     
     output$colors <- renderUI({
         radioButtons("colors", "Sample Colors", choices = c("repeat", "rainbow", "random"), selected = "repeat")
     })
 
+    
     output$saturation <- renderUI({
         req(input$colors %in% c("random", "rainbow"))
         sliderInput("saturation", "Saturation", 0, 1, value = .4, step = .1, ticks = FALSE)
     })
+    
     
     output$value <- renderUI({
         req(input$colors %in% c("random", "rainbow"))
         sliderInput("value", "Value", 0, 1, value = .9, step = .1, ticks = FALSE)
     })
     
+    
     all_targets = reactive({
         eStream_data() %>% pull(`Target Name`) %>% unique()
     })
 
+    
     output$targets <- renderUI({
         checkboxGroupInput("targets", "Targets", choices = all_targets(), selected = all_targets())
     })
     
+    
     observeEvent(input$select_all_targets, {
         updateSelectInput(session = session, "targets", selected = all_targets())
         })
+    
     
     output$template_table <- DT::renderDataTable({
         t_col_names = template_data() %>% pull('Target Color')
@@ -151,6 +167,7 @@ shinyServer(function(input, output, session) {
             formatStyle('Target Color', backgroundColor = styleEqual(t_col_names, t_col_hex)) %>%
             formatStyle('Sample Color', backgroundColor = styleEqual(s_col_names, s_col_hex))
     })
+    
     
     output$template_file <- downloadHandler(
         filename = function() {
