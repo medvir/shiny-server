32c239d2223e814d3d19cd6355a8b5e735bced1c
sschmutz <stefan.schmutz@outlook.com>
2018-12-07 07:29:41 +0100

    restructure IAVapp

--- IMV/IAVapp/app.R
+++ IMV/IAVapp/app.R
@@ -27,21 +27,15 @@ shinyApp(
                         mainPanel(
                                 tabsetPanel(
                                         tabPanel("Quantitative",
-                                                 verticalLayout(h2("IAV titer by status"),
-                                                                plotOutput("plot_quant", height = plot_height)
-                                                 )),
-                                        tabPanel("Qualitative",
-                                                 verticalLayout(h2("IAV titer by status"),
-                                                                plotOutput("plot_qual", height = plot_height)
+                                                 verticalLayout(h2("IAV titer by vaccine status"),
+                                                                plotOutput("plot_quant", height = plot_height),
+                                                                h2("IAV titer of non vaccinated by influenza status"),
+                                                                plotOutput("plot_quant_2", height = plot_height)
                                                  )),
                                         tabPanel("Sex",
                                                  verticalLayout(h2("IAV titer in male and female individuals"),
                                                                 plotOutput("plot_sex", height = plot_height)
-                                                 )),
-                                        tabPanel("Age",
-                                                verticalLayout(h2("IAV titer by age"),
-                                                               plotOutput("plot_age", height = plot_height)
-                                                ))
+                                                 ))
                                 ),
                                 hr(),
                                 h2("Data table"),
@@ -56,7 +50,7 @@ shinyApp(
                 results = reactive({
                         rbind(read_excel("2018-11-30_HI-titers.xlsx")) %>%
                                 rename(sample_name = number) %>%
-                                gather(key = measurment, value = titer, -sample_name, -virus) %>%
+                                gather(key = measurment, value = titer, -sample_name, -virus, -virus_strain) %>%
                                 select(-measurment) %>%
                                 filter(!(is.na(titer))) %>%
                                 group_by(sample_name, virus) %>%
@@ -66,13 +60,18 @@ shinyApp(
                                 ungroup() %>%
                                 mutate(result = ifelse(IAV_titer >= 4, "positive", "negative")) %>%
                                 left_join(., demo_data, by = "sample_name") %>%
+                                mutate(vaccinated_status = case_when(
+                                        influenza_status == 1 ~ "never vaccinated",
+                                        influenza_status == 2 ~ "vaccinated",
+                                        influenza_status == 3 ~ "never vaccinated",
+                                        influenza_status == 4 ~ "vaccinated",
+                                        TRUE ~ "other")) %>%
                                 mutate(influenza_status = case_when(
-                                        influenza_status == 1 ~ "never influenza, never vaccinated",
-                                        influenza_status == 2 ~ "never influenza, vaccinated",
-                                        influenza_status == 3 ~ "influenza, never vaccinated",
-                                        influenza_status == 4 ~ "influenza, vaccinated",
-                                        TRUE ~ "other")
-                                       )
+                                        influenza_status == 1 ~ "never influenza",
+                                        influenza_status == 2 ~ "never influenza",
+                                        influenza_status == 3 ~ "influenza",
+                                        influenza_status == 4 ~ "influenza",
+                                        TRUE ~ "other")) 
                 })
                 
                 output$data_table = renderTable({
@@ -84,28 +83,37 @@ shinyApp(
                 
                 output$plot_quant = renderPlot({
                         req(input$results$datapath)
-                        p = ggplot(results(), aes(x = virus, y = log2(IAV_titer), colour = influenza_status , fill = influenza_status)) +
+                        p = ggplot(results(), aes(x = fct_relevel(virus, "current H3", "current H1", "older H1"), y = log2(IAV_titer), colour = vaccinated_status , fill = vaccinated_status)) +
+                                geom_boxplot(outlier.color = "white", alpha = 0.3) +
+                                geom_jitter(height = 0, width = 0.2, size = 4) +
+                                facet_grid(. ~ vaccinated_status, scales = "free") +
+                                panel_border() + background_grid(major = "y", minor = "") +
+                                xlab("") +
+                                ylab("IAV titer (log2)") +
+                                scale_color_manual(values = c("#E69F00", "#56B4E9")) +
+                                scale_fill_manual(values = c("#E69F00", "#56B4E9")) +
+                                geom_hline(aes(yintercept = log2(40)), linetype = 2) +
+                                geom_hline(aes(yintercept = log2(10)), linetype = 2)
+                        p = p + plot_theme
+                        return(p)
+                })
+                
+                output$plot_quant_2 = renderPlot({
+                        req(input$results$datapath)
+                        p = 
+                                results() %>%
+                                filter(vaccinated_status == "never vaccinated") %>%
+                                ggplot(aes(x = fct_relevel(virus, "current H3", "current H1", "older H1"), y = log2(IAV_titer), colour = vaccinated_status , fill = vaccinated_status)) +
                                 geom_boxplot(outlier.color = "white", alpha = 0.3) +
                                 geom_jitter(height = 0, width = 0.2, size = 4) +
                                 facet_grid(. ~ influenza_status, scales = "free") +
                                 panel_border() + background_grid(major = "y", minor = "") +
                                 xlab("") +
                                 ylab("IAV titer (log2)") +
-                                theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
-                                theme(strip.text.x = element_text(size = 15))
-                        p = p + plot_theme
-                        return(p)
-                })
-                
-                output$plot_qual = renderPlot({
-                        req(input$results$datapath)
-                        p = ggplot(results(), aes(x = result, colour = result , fill = result)) +
-                                geom_bar(alpha = 0.5) +
-                                facet_grid(virus ~ influenza_status) +
-                                panel_border() + background_grid(major = "y", minor = "") +
-                                xlab("") +
-                                ylab("Number of samples") +
-                                theme(strip.text.x = element_text(size = 15))
+                                scale_color_manual(values = c("#E69F00", "#56B4E9")) +
+                                scale_fill_manual(values = c("#E69F00", "#56B4E9")) +
+                                geom_hline(aes(yintercept = log2(40)), linetype = 2) +
+                                geom_hline(aes(yintercept = log2(10)), linetype = 2)
                         p = p + plot_theme
                         return(p)
                 })
@@ -117,20 +125,9 @@ shinyApp(
                                 geom_jitter(height = 0, width = 0.2, size = 4) +
                                 #facet_grid(virus ~ influenza_status) +
                                 panel_border() + background_grid(major = "y", minor = "") +
+                                scale_color_manual(values = c("#009E73", "#0072B2")) +
+                                scale_fill_manual(values = c("#009E73", "#0072B2")) +
                                 xlab("Sex") +
-                                ylab("IAV titer (log2)")
-                        p = p + plot_theme
-                        return(p)
-                })
-                
-                output$plot_age = renderPlot({
-                        req(input$results$datapath)
-                        p = ggplot(results(), aes(x = age, y = log2(IAV_titer))) +
-                                geom_point(size = 4) +
-                                geom_smooth(method = lm, se = FALSE, fullrange = TRUE) +
-                                #facet_grid(virus ~ influenza_status) +
-                                panel_border() + background_grid(major = "y", minor = "") +
-                                xlab("Age") +
                                 ylab("IAV titer (log2)")
                         p = p + plot_theme
                         return(p)
