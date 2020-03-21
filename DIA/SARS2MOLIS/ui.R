d68b96fede5489f1bd8dc94ab5edbf8c76fc2305
sschmutz <stefan.schmutz@outlook.com>
2020-03-21 10:01:36 +0100

    remove not needed code

--- DIA/SARS2MOLIS/ui.R
+++ DIA/SARS2MOLIS/ui.R
@@ -13,14 +13,13 @@ shinyUI(fluidPage(
         column(2,
                wellPanel(
                    fileInput("pcr_file", "PCR raw data [.xlsx]:", accept = c(".xls",".xlsx")),
-                   #uiOutput("folder_selection"),
-                   #uiOutput("file_selection"),
-                   #fileInput("pcr_file_upload", "PCR Export Datei [.xlsx]:", accept = ".xls"),
                    uiOutput("target_selection"),
                    textInput("neg_control", "Negativkontrolle", value = "NC"),
                    textInput("dna_pos_control", "DNA Positivkontrolle", value = "pos DNA"),
                    textInput("rna_pos_control", "RNA Positivkontrolle", value = "pos RNA")
-               )),
+                   )
+               ),
+        
         column(2,
                wellPanel(
                    uiOutput("lin_log_selection"),
@@ -29,32 +28,18 @@ shinyUI(fluidPage(
                    uiOutput("min_delta_Rn_selection"),
                    downloadButton("molis_export", "MOLIS Export"),
                    downloadButton("pdf_export", "PDF Export")
-               )
-        ),
+                   )
+               ),
+        
         column(8,
                plotOutput("run_plot")
-        )
-    ),
+               )
+        ),
     
     fluidRow(
         hr(),
-        # column(4,
-        #        uiOutput("sample_selection"),
-        #        plotOutput("curve"),
-        #        tableOutput("fit_data_table")
-        #        ),
         column(8,
                dataTableOutput("results")
+               )
         )
-    )
-    
-    # fluidRow(
-    #     hr(),
-    #     column(12,
-    #            h5("Export"),
-    #            #downloadButton("pdf_export", "PDF Export"),
-    #            #h6(""),
-    #            downloadButton("molis_export", "MOLIS Export")
-    #     )
-    # )
-))
+    ))
