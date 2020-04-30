e51d9e654378f96e63dbdecc946c425777f0e928
sschmutz <stefan.schmutz@outlook.com>
2020-04-30 20:14:21 +0200

    add input check

--- DIA/SARS-CoV-2_serology/app.R
+++ DIA/SARS-CoV-2_serology/app.R
@@ -22,9 +22,9 @@ ui <- fluidPage(
                    hr(),
                    fileInput("barcodes_file", "Barcodes file [.xlsx]:", accept = c(".xlsx")),
                    br(),
-                   downloadButton("export_png", "Save Table (png)"),
-                   downloadButton("export_pdf", "Save Table (pdf)"),
-                   downloadButton("export_csv", "Save Table (csv)")
+                   downloadButton("export_png", "Save as png"),
+                   downloadButton("export_pdf", "Save as pdf"),
+                   downloadButton("export_csv", "Save as csv")
                    )
                ),
 
@@ -42,10 +42,14 @@ server <- function(input, output, session) {
     table <- reactive({
         
         all_in_one_path = input$all_in_one_file$datapath
+        all_in_one_name = input$all_in_one_file$name
 
         igg_path = input$igg_file$datapath
+        igg_name = input$igg_file$name
         iga_path = input$iga_file$datapath
+        iga_name = input$iga_file$name
         igm_path = input$igm_file$datapath
+        igm_name = input$igm_file$name
 
         barcodes_path = input$barcodes_file$datapath
 
@@ -57,6 +61,57 @@ server <- function(input, output, session) {
         # iga_path <- "data/200420_plate_1_IgA_20200420_122957.csv"
         # igm_path <- "data/200420_plate_1_IgM_20200420_125709.csv"
         
+        
+
+        # check input -------------------------------------------------------------
+        
+        
+        if (!is.null(input$all_in_one_file$datapath)) {
+            date_all_in_one <- strsplit(all_in_one_name, "_")[[1]][1]
+            date_barcodes <- strsplit(input$barcodes_file$name, "_")[[1]][1]
+            
+            if (date_all_in_one != date_barcodes) {
+                stop("Check uploaded Files (dates do not match)")
+            }
+        } else {
+            date_igg <- strsplit(igg_name, "_")[[1]][1]
+            plate_igg <- paste0(strsplit(igg_name, "_")[[1]][2:3], collapse = " ")
+            
+            if (isFALSE(str_detect(igg_name, pattern = regex("igg", ignore_case = TRUE)))){
+                stop("Check Luminex output for IgG ('IgG' is not part of the filename)")
+            }
+            
+            
+            date_iga <- strsplit(input$iga_file$name, "_")[[1]][1]
+            plate_iga <- paste0(strsplit(input$iga_file$name, "_")[[1]][2:3], collapse = " ")
+            
+            if (isFALSE(str_detect(iga_name, pattern = regex("iga", ignore_case = TRUE)))){
+                stop("Check Luminex output for IgA ('IgA' is not part of the filename)")
+            }
+            
+            
+            date_igm <- strsplit(input$igm_file$name, "_")[[1]][1]
+            plate_igm <- paste0(strsplit(input$igm_file$name, "_")[[1]][2:3], collapse = " ")
+            
+            if (isFALSE(str_detect(igm_name, pattern = regex("igm", ignore_case = TRUE)))){
+                stop("Check Luminex output for IgM ('IgM' is not part of the filename)")
+            }
+            
+            date_barcodes <- strsplit(input$barcodes_file$name, "_")[[1]][1]
+            
+            
+            date = c(date_igg, date_iga, date_igm, date_barcodes)
+            plate = c(plate_igg, plate_iga, plate_igm)
+            
+            if (length(unique(date)) != 1){
+                stop("Check uploaded Files (not all dates are equal)")
+            }
+            
+            if (length(unique(plate)) != 1){
+                stop("Check uploaded Files (not all plate numbers are equal)")
+            }
+        }
+
         
         # Prepare Barcodes --------------------------------------------------------
         
@@ -90,8 +145,6 @@ server <- function(input, output, session) {
         }
 
 
-        
-        
         # set flag ----------------------------------------------------------------
         
         # minimal count for each measurment has to be following value, everything below gets flagged
@@ -121,19 +174,32 @@ server <- function(input, output, session) {
                    IgM_Resultat = if_else(IgM_Resultat, "pos", "neg"))
         
         net_mfi_foc_clean
-        
-
+    })
+    
+    assay_date <- reactive({
+        if (!is.null(input$all_in_one_file$datapath)) {
+            strsplit(all_in_one_name, "_")[[1]][1]
+        } else {
+            strsplit(igg_name, "_")[[1]][1]
+        }
+    })
+    
+    plate_number <- reactive({
+        if (!is.null(input$all_in_one_file$datapath)) {
+            paste0(strsplit(all_in_one_name, "_")[[1]][2:3], collapse = "_")
+        } else {
+            paste0(strsplit(igg_name, "_")[[1]][2:3], collapse = "_")
+        }
     })
     
     
-        
-        table_gt <- reactive({
+    table_gt <- reactive({
             create_gt(table())
     })
 
-    output$table <- render_gt(
-        expr = table_gt()
-    )
+    
+    output$table <- render_gt(expr = table_gt())
+    
     
     output$export_png <- downloadHandler(
         filename = function() {
@@ -141,7 +207,6 @@ server <- function(input, output, session) {
         },
         content = function(file) {
             gtsave(table_gt(), file, vwidth = 1500)
-            
         }
     )
     
